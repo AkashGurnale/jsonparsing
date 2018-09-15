84b1ca6f3bb8d7c6775da33e7651f8b72609cef2
Felix ITs 04 <felixits04@Student-P-04.local>
2018-09-15 14:10:16 +0530

    First Commit

---
+++ Day 10 - GitRepository/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Day 10 - GitRepository
+//
+//  Created by Felix ITs 04 on 15/09/18.
+//  Copyright © 2018 Akash. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
